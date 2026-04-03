3b3397575b4dccb058f277f9c0cc36f963bc070d
JohannVink <johann.vink04@gmail.com>
2026-04-04 01:56:08 +0300

    Add simple level up UI, game manager, import TextMeshPro

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    private int currentLevel = 1;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void Update()
+    {
+        if (Keyboard.current.lKey.wasPressedThisFrame)
+            TriggerLevelUp();
+    }
+    // DENISSI XP SUSTEEM CALLIB SELLE KUI MANGIJA SAAB LEVEL UPI
+    public void TriggerLevelUp()
+    {
+        currentLevel++;
+        LevelUpUI.Instance.Show(UpgradeManager.Instance.GetUpgradeChoices());
+    }
+
+    public int GetCurrentLevel() => currentLevel;
+
+    
+}
